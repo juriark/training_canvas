b16c0ec6e865288d758672aaabec9de21304a06e
Julia Sammer <juliasammer@gmx.at>
2023-02-28 15:54:29 +0100

    Add migration script to create all necessary tables

---
+++ src/database/alembic/versions/9266010841bf_create_projects_classes_images_and_.py
@@ -0,0 +1,77 @@
+"""create projects, classes, images and checkpoints table
+
+Revision ID: 9266010841bf
+Revises: 
+Create Date: 2023-02-28 14:26:06.406735
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9266010841bf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Create projects table
+    op.create_table(
+        "projects",
+        sa.Column(name="id", type_=sa.Integer(), primary_key=True),
+        sa.Column(name="project_name", type_=sa.String(), nullable=False, unique=True)
+    )
+
+    # Create checkpoints table
+    op.create_table(
+        "checkpoints",
+        sa.Column(name="id", type_=sa.Integer(), primary_key=True),
+        sa.Column(name="project_id", type_=sa.Integer(), nullable=False),
+        sa.Column(name="checkpoint_blob_storage_uid", type_=sa.String(), nullable=False)
+    )
+    op.create_foreign_key(
+        constraint_name="fk_projects_checkpoint",
+        source_table="checkpoints",
+        referent_table="projects",
+        local_cols=["project_id"],
+        remote_cols=["id"]
+    )
+
+    # Create classes table
+    op.create_table(
+        "classes",
+        sa.Column(name="id", type_=sa.Integer(), primary_key=True),
+        sa.Column(name="label", type_=sa.String(), nullable=False),
+        sa.Column(name="project_id", type_=sa.Integer(), nullable=False)
+    )
+    op.create_foreign_key(
+        constraint_name="fk_projects_classes",
+        source_table="classes",
+        referent_table="projects",
+        local_cols=["project_id"],
+        remote_cols=["id"]
+    )
+
+    # Create images table
+    op.create_table(
+        "images",
+        sa.Column(name="blob_storage_uid", type_=sa.String(), unique=True, nullable=False),
+        sa.Column(name="classes_id", type_=sa.Integer(), nullable=False)
+    )
+    op.create_foreign_key(
+        constraint_name="fk_classes_images",
+        source_table="images",
+        referent_table="classes",
+        local_cols=["classes_id"],
+        remote_cols=["id"]
+    )
+
+
+def downgrade() -> None:
+    # Drop tables in correct order
+    op.drop_table(table_name="images")
+    op.drop_table(table_name="classes")
+    op.drop_table(table_name="checkpoints")
+    op.drop_table(table_name="projects")
