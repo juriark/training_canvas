c6d95376eb89853dbe3e52ae0d45e59bb619c679
Julia Sammer <juliasammer@gmx.at>
2023-03-01 12:26:04 +0100

    Fix missing ForeignKey in tables and __repr__ implementations

--- src/training_canvas/database/orm_models.py
+++ src/training_canvas/database/orm_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -10,15 +10,21 @@ class Projects(Base):
     id = Column(Integer, primary_key=True)
     project_name = Column(String, nullable=False, unique=True)
 
+    def __repr__(self):
+        return f"project_name = {self.project_name}"
+
 
 class Checkpoints(Base):
     __tablename__ = "checkpoints"
 
     id = Column(Integer, primary_key=True)
-    project_id = Column(Integer, nullable=False)
+    project_id = Column(Integer, ForeignKey("projects.id"))
     checkpoint_blob_storage_uid = Column(String, nullable=False, unique=True)
 
-    project = relationship("Projects", back_populates="id")
+    # project = relationship("Projects", back_populates="id")
+
+    def __repr__(self):
+        return f"project_id = {self.project_id}, checkpoint_blob_storage_uid = {self.checkpoint_blob_storage_uid}"
 
 
 class Classes(Base):
@@ -26,15 +32,21 @@ class Classes(Base):
 
     id = Column(Integer, primary_key=True)
     label = Column(String, nullable=False)
-    project_id = Column(Integer, nullable=False)
+    project_id = Column(Integer, ForeignKey("projects.id"))
 
-    project = relationship("Projects", back_populates="id")
+    # project = relationship("projects", back_populates="id")
+
+    def __repr__(self):
+        return f"label = {self.label}, project_id = {self.project_id}"
 
 
 class Images(Base):
     __tablename__ = "images"
 
     blob_storage_uid = Column(String, nullable=False, primary_key=True)
-    classes_id = Column(Integer, nullable=False)
+    classes_id = Column(Integer, ForeignKey("classes.id"))
 
-    classes = relationship("Classes", back_populates="id")
+    # classes = relationship("Classes", back_populates="id")
+
+    def __repr__(self):
+        return f"blob_storage_uid = {self.blob_storage_uid}, classes_id = {self.classes_id}"
